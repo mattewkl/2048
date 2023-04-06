5214d1b96d992164e7970873d032f97b0d70f094
mattewkl <cikle290@gmail.com>
2023-04-06 06:53:06 +0300

    basic color-change

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,5 @@ const gameBoard = document.querySelector('.game-board')
 const grid = new Grid(gameBoard);
 
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard))
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard))
 
--- scripts/tile.js
+++ scripts/tile.js
@@ -2,8 +2,7 @@ export class Tile {
   constructor(gridElement)  {
     this.tileElement = document.createElement("div");
     this.tileElement.classList.add("standartblock", "game-board__tile");
-    this.value = Math.random() > 0.5 ? 2 : 4;
-    this.tileElement.textContent = this.value;
+    this.setValue(Math.random() > 0.5 ? 2 : 4);
     gridElement.append(this.tileElement)
   }
 
@@ -12,6 +11,15 @@ export class Tile {
     this.y = y;
     this.tileElement.style.setProperty("--x", x)
     this.tileElement.style.setProperty("--y", y)
+    
+  }
+
+  setValue(value) {
+    this.value = value;
+    this.tileElement.textContent = value;
+    const bgLigthness = 100 - Math.log2(value) * 9;
+    this.tileElement.style.setProperty("--bg-lightness", `${bgLigthness}%`);
+    this.tileElement.style.setProperty("--text-lightness", `${bgLigthness <50 ? 90 : 10}%`)
     
   }
 }
